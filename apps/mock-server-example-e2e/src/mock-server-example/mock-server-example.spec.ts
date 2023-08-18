a400341deaa0a2efacf93817514d87ca1c9842d2
Navneet Nagpal <navneetnaagpal@gmail.com>
2023-08-18 15:52:23 +0530

    ADDED unit tests

--- apps/mock-server-example-e2e/src/mock-server-example/mock-server-example.spec.ts
+++ apps/mock-server-example-e2e/src/mock-server-example/mock-server-example.spec.ts
@@ -1,10 +1,26 @@
 import axios from 'axios';
 
 describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+  it('should return for test/get.json ', async () => {
+    const res = await axios.get(`/api/v1/test`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toEqual({
+      status: 'succes',
+      data: {
+        examples: 'get response example',
+      },
+    });
+  });
+  it('should return for test/post.json ', async () => {
+    const res = await axios.post(`/api/v1/test`);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual({
+      status: 'succes',
+      data: {
+        examples: 'post response example',
+      },
+    });
   });
 });
