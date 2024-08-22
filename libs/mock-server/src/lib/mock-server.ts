f2e317fc01b6ebff3bbb4557f69a52423c4021e3
Navneet Nagpal <navneetnaagpal@gmail.com>
2024-08-22 14:39:48 +0530

    Added dynamic route

--- libs/mock-server/src/lib/mock-server.ts
+++ libs/mock-server/src/lib/mock-server.ts
@@ -6,6 +6,7 @@ import errors from './error';
 class MockServer {
   config: any;
   app: any;
+  allEndpoints: string[];
   fse: any;
 
   constructor({ fse = fs, ...options }: any = {}, app) {
@@ -19,22 +20,94 @@ class MockServer {
     this.fse = fse;
   }
 
-  ensureNoSlash(path) {
+  ensureNoSlashEnd(path) {
     return path.substr(path.length - 1, 1) === '/'
       ? path.substr(0, path.length - 1)
       : `${path}`;
   }
+  ensureNoSlashStart(path) {
+    return path.substr(0, 1) === '/'
+      ? path.substr(1, path.length - 1)
+      : `${path}`;
+  }
+
+  makePath(
+    dir,
+    parentPath = this.config.staticApiPath,
+    _arrPaths: string[] = [],
+    processPath = ''
+  ) {
+    const arrPaths: string[] = _arrPaths;
+    const thisPath = path.join(parentPath, dir);
+    const pathProcessed = parentPath
+      ? fs.statSync(thisPath).isDirectory() &&
+        !fs.existsSync(path.join(thisPath, 'index.js'))
+        ? path.join(processPath, dir)
+        : processPath
+      : dir;
+    if (fs.statSync(thisPath).isDirectory()) {
+      const result = fs.readdirSync(thisPath);
+      if (result.length > 0) {
+        result.map((childPath) => {
+          this.makePath(childPath, thisPath, arrPaths, pathProcessed);
+        });
+      }
+    } else {
+      if (arrPaths.indexOf(pathProcessed) === -1) {
+        arrPaths.push(pathProcessed);
+        console.log(`created:${pathProcessed}`);
+      }
+    }
+    return arrPaths;
+  }
+
+  async getAllUrls() {
+    const result = fs.readdirSync(this.config.staticApiPath);
+    this.allEndpoints = result.map((path) => this.makePath(path)).flat();
+  }
+
+  getFinalPath(url) {
+    const index = this.allEndpoints.indexOf(url);
+    let matchedItem = '';
+    if (index === -1) {
+      const params = {};
+      this.allEndpoints.forEach((item) => {
+        let hasMatch = true;
+        const leftSide = item.split(path.sep);
+        const rightSide = url.split('/');
+        leftSide.forEach((item, idx) => {
+          if (hasMatch && item.match(new RegExp('\\[.*\\]'))) {
+            params[item.match(new RegExp('\\[.*\\]'))[1]] = rightSide[idx];
+          } else if (hasMatch) {
+            hasMatch = item === rightSide[idx];
+          }
+        });
+        if (hasMatch) {
+          matchedItem = item;
+        }
+      });
+    }
+    if (matchedItem) {
+      return path.resolve(
+        this.ensureNoSlashEnd(this.config.staticApiPath),
+        matchedItem
+      );
+    }
+    return path.resolve(this.ensureNoSlashEnd(this.config.staticApiPath), url);
+  }
 
   init() {
     console.log('--- init mock-server');
+    this.getAllUrls();
     this.app.all(this.config.staticMocks, (req, res) => {
       const reqPath = req._parsedUrl.pathname;
       const method = req.method.toLowerCase();
-      let finalPath = reqPath.resolve(
-        this.ensureNoSlash(this.config.staticApiPath),
-        reqPath
-      );
-      // console.log(finalPath);
+      const urlToServe = this.ensureNoSlashStart(reqPath);
+
+      // console.log(reqPath);
+      console.log(`looking for=${urlToServe}`);
+
+      let finalPath = this.getFinalPath(urlToServe);
       let response: any = new Response(errors.notfound()).error();
       let isJs = false;
       if (this.fse.existsSync(path.join(finalPath, method, 'index.js'))) {
@@ -51,8 +124,12 @@ class MockServer {
           console.log(`executing dynamic route`);
           response = require(finalPath);
         } else {
-          response = this.fse.readFileSync(finalPath, 'utf8');
-          response = JSON.parse(response);
+          if (fs.existsSync(finalPath)) {
+            response = this.fse.readFileSync(finalPath, 'utf8');
+            response = JSON.parse(response);
+          } else {
+            throw `${urlToServe} not found`;
+          }
         }
         if (typeof response === 'function') {
           response = response(req, res);
